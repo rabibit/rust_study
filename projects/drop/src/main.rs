9335caf11e12a31651f89cc921c59192d6511ae8
wangyouquan <rabibit@163.com>
2021-05-16 17:13:19 +0800

    添加Drop相关examples

---
+++ projects/drop/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    drop(c); // call drop early
+    println!("Hello, world!");
+}
+
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data '{}'", self.data);
+    }
+}
