adf1cf72790a34c29cf0b5b281e770dd218190c5
wangyouquan <rabibit@163.com>
2021-05-17 21:26:45 +0800

    添加面向对象编程相关examples

---
+++ projects/object_oriented_programming/src/main.rs
@@ -0,0 +1,41 @@
+use object_oriented_programming::{Button, Screen, Draw};
+
+fn main() {
+    let screen = Screen {
+        components:  vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No"),
+                ],
+            }),
+
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+
+    };
+    println!("Hello, world!");
+
+    screen.run();
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        println!("draw select box");
+        println!("width: {}, height: {}, option: {:?}",
+                  self.width, self.height, self.options);
+    }
+}
